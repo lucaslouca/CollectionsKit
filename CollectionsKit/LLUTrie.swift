fa492d4c126ad4802c6de8a20e7a63e58bc42e18
Lucas Louca <lucaslouca@users.noreply.github.com>
2016-02-11 11:44:32 +0100

    Update LLUTrie.swift

--- CollectionsKit/LLUTrie.swift
+++ CollectionsKit/LLUTrie.swift
@@ -3,18 +3,30 @@ public class LLUTrie {
     private var prefixCount:Int = 0
     private var edges:[Character: LLUTrie]
     
+    /// All words in the trie
     public var words:[String] {
         return self.wordsWithPrefix("")
     }
     
+    /// Creates a new LLUTrie
     public init() {
         edges = [:]
     }
     
+    /// Returns true if the trie contains the specified word.
+    ///
+    /// - Parameters:
+    ///     - word: The word to search
+    /// - Returns: true if the trie contains the specified word
     public func containsWord(word:String) -> Bool {
         return countWords(word) > 0
     }
     
+    /// Returns the number of times the specified word appears in the trie.
+    ///
+    /// - Parameters:
+    ///     - word: The word to search
+    /// - Returns: Number of times the specified word appears in the trie
     public func countWords(word:String) -> Int {
         if (word.characters.count == 0) {
             return self.wordCount
@@ -28,6 +40,11 @@ public class LLUTrie {
         }
     }
     
+    /// Returns the number of times the specified prefix appears in the trie.
+    ///
+    /// - Parameters:
+    ///     - prefix: The prefix to search
+    /// - Returns: Number of times the specified prefix appears in the trie
     public func countPrefixes(prefix:String) -> Int {
         if (prefix.characters.count == 0) {
             return self.prefixCount
@@ -41,6 +58,10 @@ public class LLUTrie {
         }
     }
     
+    /// Inserts the specified word into this trie.
+    ///
+    /// - Parameters:
+    ///     - word: The word to add
     public func add(word:String) {
         if (word.characters.count == 0) {
             self.wordCount++
@@ -82,6 +103,11 @@ public class LLUTrie {
         }
     }
     
+    /// Decreases the occurrences of the specified word in the trie by 1.
+    ///
+    /// - Parameters:
+    ///     - word: The word to decreases its occurrence
+    /// - Returns: true if the word occurrence has been decreased
     public func removeWord(word:String) -> Bool {
         if (word.characters.count == 0) {
             --wordCount
@@ -103,6 +129,11 @@ public class LLUTrie {
         }
     }
     
+    /// Retrieves all words that start with the specified prefix.
+    ///
+    /// - Parameters:
+    ///     - prefix: The prefix to search for
+    /// - Returns: All words that start with the specified prefix
     public func wordsWithPrefix(prefix:String) -> [String] {
         return self.wordsWithPrefix(prefix,currentWord:"")
     }
